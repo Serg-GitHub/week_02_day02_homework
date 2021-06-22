2f0036bca01204c99597e0b1f0d572a69185370a
Sergio <sergio.eneide@protonmail.com>
2021-06-22 20:44:01 +0100

    testing.md

---
+++ testing_start_code/tests/calculator_test.py
@@ -0,0 +1,8 @@
+# tests/calculator_test.py
+
+import unittest
+from src.calculator import add, divide, multiply, subtract
+
+class TestCalculator(unittest.TestCase): # NEW
+    pass                                 # NEW
+
